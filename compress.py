ea1fee2a2583b8de92dfab8483cb82a141036abe
SSA1MON <zagorulko.kirill@gmail.com>
2022-12-24 00:58:06 +0300

    comments have been changed. added directory connection timeout

--- compress.py
+++ compress.py
@@ -1,23 +1,36 @@
 import os
+import concurrent.futures
+import config as cfg
+
 from pathlib import Path
-from time import time, sleep
+from time import monotonic
 from datetime import datetime
 from loguru import logger
-from config import img_path, app, postfix, creation_time, quality, image_format, log_name
 
-logger.add(f"logs/{log_name}.log", format="{time:DD.MM.YYYY HH:mm:ss} | {level} | {message}", level="DEBUG",
+logger.add(f"logs/{cfg.log_name}.log", format="{time:DD.MM.YYYY HH:mm:ss} | {level} | {message}", level="DEBUG",
            rotation="1 day", compression="zip")
+logger.add(f"logs/{cfg.log_name}_error.log", format="{time:DD.MM.YYYY HH:mm:ss} | {level} | {message}", level="ERROR",
+           rotation="1 day", compression="zip")
+
+
+@logger.catch
+def timeout(path, pool):
+    future = pool.submit(os.listdir, path)
+    try:
+        return future.result(cfg.timeout_time)
+    except concurrent.futures.TimeoutError:
+        return None
 
 
 @logger.catch
 def creation_time_check(file_path: str) -> int:
     """
-    Функция проверки даты создания. Проверяет дату создания файла по полученному пути.
+    Function of checking creation date. Checks the creation date of the file from the received path.
 
     Parameters:
-        file_path (str): Путь к файлу.
+        file_path (str): Path to the file.
     Returns:
-        res (int): Количество прошедших дней от текущего времени до даты создания файла.
+        res (int): Number of days elapsed from the current time to the date of file creation.
     """
 
     current_time = datetime.now()
@@ -33,79 +46,88 @@ def creation_time_check(file_path: str) -> int:
 @logger.catch
 def images_compress(path: str, renamed: int = 0, compressed: int = 0) -> tuple[int, int]:
     """
-    Функция сжатия. Сжимает файлы-изображения во всех вложенных директориях из полученного пути.
+    Compression function. Compresses image files in all nested directories from the received path.
 
     Parameters:
-        path (str): Путь до директории.
-        renamed (int): Переменная для подсчета количества переименованных файлов.
-        compressed (int): Переменная для подсчета количества сжатых файлов.
+        path (str): Path to the directory.
+        renamed (int): Variable for counting the number of renamed files.
+        compressed (int): Variable for counting the number of compressed files.
     Returns:
-        renamed (int): Количество переименованных файлов.
-        compressed (int): Количество сжатых файлов.
+        renamed (int): Number of renamed files.
+        compressed (int): Number of compressed files.
     """
 
-    logger.info("Current directory: " + str(path))
+    short_path = str(path)[str(path).find(Path.cwd().stem):]
+    logger.info(f"Current directory \"{short_path}\"")
 
-    for i in os.listdir(path=path):
-        iter_path = Path(path, i)
-        extension = i.index(i[- (i[::-1].find('.')) - 1:])  # Расширение файла в виде индекса
+    # Checking connection to the directory
+    pool = concurrent.futures.ThreadPoolExecutor()
+    available_path = timeout(path=path, pool=pool)
 
-        # Проверка наличия директории и игнорирование файлов с постфиксом в имени
-        if Path.exists(iter_path) and postfix not in i:
-            # Рекурсивный проход по всем директориям внутри пути
-            if Path.is_dir(iter_path):
-                logger.info("Going to " + str(iter_path))
-                res = images_compress(path=str(iter_path), renamed=renamed, compressed=compressed)
-                renamed, compressed = res[0], res[1]
-                logger.info("Back to " + str(path))
-                continue
+    if available_path is not None:
+        for i in available_path:
+            iter_path = Path(path, i)
+            extension = i.index(i[-(i[::-1].find(".")) - 1:])  # File extension index
 
-            # Проверка, что объект - файл и имеет заданные расширения в своем имени
-            if not Path.is_dir(iter_path) and i[extension:] in image_format:
-                file_date = creation_time_check(str(iter_path))
-
-                # Проверка, что время создания файла соответствует условию
-                if file_date < creation_time:
-                    logger.info(f'>>> "{i}" does not meet the condition ({file_date}/{creation_time} days)')
+            # Checking for a directory and ignoring files with a postfix in the name
+            if Path.exists(iter_path) and cfg.postfix not in i:
+                # Recursive traversal through all directories inside the path
+                if Path.is_dir(iter_path):
+                    logger.info(f"Going to \"{short_path}\"")
+                    res = images_compress(path=str(iter_path), renamed=renamed, compressed=compressed)
+                    renamed, compressed = res[0], res[1]
+                    logger.info(f"Back to \"{short_path}\"")
                     continue
 
-                # Поиск и замена пробелов в имени файла
-                if not Path.is_dir(iter_path) and ' ' in i and not Path.exists(Path(path, i.replace(' ', '-'))):
-                    new_filename = i.replace(' ', '-')
-                    os.rename(src=iter_path, dst=str(Path(path, new_filename)))
-                    logger.info(f'>>> "{i}" was renamed to "{new_filename}"')
-                    i = new_filename
-                    iter_path = Path(path, i)
-                    renamed += 1
+                # Checking that the object is a file and has the specified extensions in the name
+                if not Path.is_dir(iter_path) and i[extension:] in cfg.image_format:
+                    file_date = creation_time_check(str(iter_path))
+                    new_filename = i.replace(" ", "-")
 
-                # Сжатие файла mozjpeg`ом
-                exec_time = time()
-                new_filename = i[:extension] + postfix + i[extension:]
-                logger.info(f"In the process of compression: {i}")
-                os.system(
-                    command=f'{app} -quality {quality} -progressive -outfile "{Path(path, new_filename)}" "{iter_path}"'
-                )
+                    # Checking that the file creation time matches condition
+                    if file_date < cfg.creation_time:
+                        logger.info(f"  \"{i}\" does not meet the condition ({file_date}/{cfg.creation_time} days)")
+                        continue
 
-                # Проверка на существование сжатого файла и удаление оригинала
-                if Path.exists(Path(path, new_filename)):
-                    os.remove(path=iter_path)
-                    exec_time = round(time() - exec_time, 2)
-                    logger.info(f">>> {i} was compressed in {exec_time} seconds.")
-                    compressed += 1
+                    # Finding and replacing spaces in the file name
+                    if " " in i and not Path.exists(Path(path, new_filename)):
+                        os.rename(src=iter_path, dst=str(Path(path, new_filename)))
+                        logger.info(f"  \"{i}\" was renamed to \"{new_filename}\"")
+                        i = new_filename
+                        iter_path = Path(path, i)
+                        renamed += 1
+
+                    # File compression using mozjpeg
+                    exec_time = monotonic()
+                    new_filename = i[:extension] + cfg.postfix + i[extension:]
+                    logger.info(f"In the process of compression: {i}")
+                    os.system(
+                        command=f"{cfg.app} -quality {cfg.quality} -progressive "
+                                f"-outfile \"{Path(path, new_filename)}\" \"{iter_path}\""
+                    )
+
+                    # Checking for the existence of a compressed file and deleting the original
+                    if Path.exists(Path(path, new_filename)):
+                        os.remove(path=iter_path)
+                        exec_time = round(monotonic() - exec_time, 2)
+                        logger.info(f"  {i} was compressed in {exec_time} seconds.")
+                        compressed += 1
+                else:
+                    logger.warning(f"This is not an image: {Path(short_path, i)}")
             else:
-                logger.warning("This is not an image: " + str(iter_path))
-        else:
-            logger.warning("Wrong name or path does not exist: " + str(iter_path))
-    return renamed, compressed
+                logger.warning(f"Wrong name or path does not exist: {Path(short_path, i)}")
+        return renamed, compressed
+    else:
+        logger.error(f"Path \"{short_path}\" is unavailable (timeout {cfg.timeout_time} sec). Break...")
+        return renamed, compressed
 
 
 if __name__ == "__main__":
     try:
-        logger.info('Starting script...')
-        sleep(1)
-        uptime = time()
-        result = images_compress(path=img_path)
-        uptime = round(time() - uptime, 2)
-        logger.success(f'Script finished. Renamed: {result[0]} Compressed: {result[1]} Time: {uptime} sec.')
+        logger.info("Starting script...")
+        uptime = monotonic()
+        result = images_compress(path=cfg.img_path)
+        uptime = round(monotonic() - uptime, 2)
+        logger.success(f"Script finished. Renamed: {result[0]} Compressed: {result[1]} Time: {uptime} sec.")
     except Exception as ex:
         logger.critical(ex)
