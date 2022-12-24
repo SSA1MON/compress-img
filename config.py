1c3a5491d3cd582a5b1c0cbf3d9b7b16fb57b106
SSA1MON <zagorulko.kirill@gmail.com>
2022-12-24 19:17:59 +0300

    update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 img_path = Path("C:/Users/LocalUser/Desktop")
-app = Path(Path.cwd(), 'mozjpeg/cjpeg')
+app = Path(Path.cwd(), "mozjpeg/cjpeg")
 image_format = [".jpg", ".jpeg", ".png"]
 log_name = "compress"
 postfix = "_compressed"
-quality = "50"
-creation_time = 0  # days
+quality = "40"
+timeout_time = 1    # time to establish a connection to the directory in seconds (0 - instant interruption)
+creation_time = 0    # days
